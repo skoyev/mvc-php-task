73ce37345ecd689ab8d3a1d4048153a1975d9a40
skoev <skoev@hotmail.com>
2018-03-23 03:12:41 -0300

    Project uploaded

--- application/controllers/task/TaskController.class.php
+++ application/controllers/task/TaskController.class.php
@@ -23,6 +23,51 @@ class TaskController extends Controller{
     }
 
     /**
+     * Upload task image file function.
+     */
+    public function uploadTaskFileAction() {
+
+      $currentDir = getcwd();
+          $uploadDirectory = "uploads\\";
+
+          $errors = []; // Store all foreseen and unforseen errors here
+
+          $fileExtensions = ['jpeg','jpg','png']; // Get all the file extensions
+          $fileName = $_FILES['fileToUpload']['name'];
+          $fileSize = $_FILES['fileToUpload']['size'];
+          $fileTmpName  = $_FILES['fileToUpload']['tmp_name'];
+          $fileType = $_FILES['fileToUpload']['type'];
+          $tmp = explode('.', $fileName);
+          $fileExtension = end($tmp);
+
+          $uploadPath = ROOT . $uploadDirectory . basename($fileName);
+
+          if (! in_array($fileExtension, $fileExtensions)) {
+              $errors[] = "This file extension is not allowed. Please upload a JPEG or PNG file";
+          }
+
+          if ($fileSize > 3000000) {
+              $errors[] = "This file is more than 2MB. Sorry, it has to be less than or equal to 2MB";
+          }
+
+          $result = array();
+          if (empty($errors)) {
+
+              $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+
+              if ($didUpload) {
+                $result = array('status'=>'done', 'message'=>"The file $fileName has been uploaded successfully!.");
+              } else {
+                $result = array('status'=>'error', 'message'=>"An error occurred somewhere. Try again or contact the admin");
+              }
+          } else {
+            $result = array('status'=>'error', 'message'=>$errors);
+          }
+
+          echo json_encode($result);
+    }
+
+    /**
      * Update an existing tag by task id
      */
     public function updateTaskAction(){
@@ -44,7 +89,7 @@ class TaskController extends Controller{
         if (isset($_POST['userName'])) {
           $task = array("user_name" => $_POST['userName'],
           "email" => $_POST['email'], "task_details" => $_POST['task-details'],
-          "status" => "Inactive");
+          "status" => "Inactive", "task_image" => $_POST['task-image']);
 
           $taskModel = new TaskModel("tasks");
           $taskModel->insert( $task );
--- framework/core/Query.class.php
+++ framework/core/Query.class.php
@@ -5,8 +5,8 @@ define("ORDER_BY_ID", "ORDER_BY_ID");
 define("ORDER_BY_EMAIL", "ORDER_BY_EMAIL");
 define("ORDER_BY_USER_NAME", "ORDER_BY_USER_NAME");
 
-define("FETCH_ALL_TASKS_QUERY_ORDER_BY_ID", "select id, user_name, email, task_details, status from tasks order by id desc limit :maxRecords offset :offset");
-define("FETCH_ALL_TASKS_QUERY_ORDER_BY_EMAIL", "select id, user_name, email, task_details, status from tasks order by email asc limit :maxRecords offset :offset");
-define("FETCH_ALL_TASKS_QUERY_ORDER_BY_USER_NAME", "select id, user_name, email, task_details, status from tasks order by user_name asc limit :maxRecords offset :offset");
-define("FETCH_ONE_TASK_BY_ID_QUERY", "select id, user_name, email, task_details, status from tasks where id = ?");
+define("FETCH_ALL_TASKS_QUERY_ORDER_BY_ID", "select id, user_name, email, task_details, status, task_image from tasks order by id desc limit :maxRecords offset :offset");
+define("FETCH_ALL_TASKS_QUERY_ORDER_BY_EMAIL", "select id, user_name, email, task_details, status, task_image from tasks order by email asc limit :maxRecords offset :offset");
+define("FETCH_ALL_TASKS_QUERY_ORDER_BY_USER_NAME", "select id, user_name, email, task_details, status, task_image from tasks order by user_name asc limit :maxRecords offset :offset");
+define("FETCH_ONE_TASK_BY_ID_QUERY", "select id, user_name, email, task_details, status, task_image from tasks where id = ?");
 define("FETCH_TOTAL_TASKS_COUNT", "select count(id) total from tasks limit 5000");
